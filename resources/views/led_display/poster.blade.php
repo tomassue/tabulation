6d14bd1b74127f11e91dbfce91801eb9d086247b
cmisidcdo <109932089+cmisidcdo@users.noreply.github.com>
2025-06-26 13:59:27 +0800

    fix on display images
    fix on display images

--- resources/views/led_display/poster.blade.php
+++ resources/views/led_display/poster.blade.php
@@ -84,102 +84,47 @@
             <div class="col-md-2 d-flex align-items-center justify-content-center">
                 <img src="{{ $image[$index] }}" class="img-fluid" style="max-height: 120px;" alt="1st Place">
             </div>
-            
             <div class="col-md-10 d-flex align-items-center">
                 <div class="fw-bold" style="font-size: {{$font[$index]}}; color:{{$color[$index]}};">
                     #{{$item->participant_no}} 
-
-                   
-
                     <button type="button" class="btn show-poster fw-bold"
                             data-bs-toggle="modal"
-                            data-bs-target="#fullscreenModal"
-                            data-image="{{ asset('storage/posters/' . $item->output_file) }}"
+                            data-bs-target="#fullscreenModal{{ $item->id }}"
                             style="color: {{ $color[$index] }}; text-decoration: none; font-size: {{$font[$index]}};">
                         <i>{{ $item->participant }}</i>
                     </button>
-
-
-
-
+                    <div class="modal fade" id="fullscreenModal{{ $item->id }}" tabindex="-1" aria-labelledby="fullscreenModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-fullscreen">
+                            <div class="modal-content bg-dark p-0 border-0"> <!-- Optional: dark background -->
+                            <div class="modal-header border-0">
+                                <h5 class="modal-title text-white" id="fullscreenModalLabel">{{$item->participant}}</h5>
+                                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body p-0 d-flex justify-content-center align-items-center">
+                                <img id="poster-image" src="{{ asset('storage/' . $item->posterOutput?->output_file) }}" alt="Poster Output" class="img-fluid w-100 h-100 object-fit-contain">
+                            </div>
+                            </div>
+                        </div>
+                    </div>
                     <a href="{{ route('display_poster_output', $item->id) }}"  target="_blank">  </a> 
                 </div>
             </div>
         </div>
         @endforeach
     </div>
-
-<div class="row my-5 px-5">
-    <div class="d-flex justify-content-center align-items-center flex-wrap gap-4">
-        <img src="{{ asset('img/cdo-seal.png') }}" alt="Logo 1" class="img-fluid" style="max-height: 150px;">
-        <img src="{{ asset('img/seallogo.png') }}" alt="Logo 2" class="img-fluid" style="max-height: 120px;">
-        <img src="{{ asset('img/risebig.png') }}" alt="Logo 3" class="img-fluid" style="max-height: 100px;">
-        <img src="{{ asset('img/logo1.png') }}" alt="Logo 4" class="img-fluid" style="max-height: 100px;">
-        <img src="{{ asset('img/tourismlogo.png') }}" alt="Logo 5" class="img-fluid" style="max-height: 100px;">
-        <img src="{{ asset('img/ictlogo.png') }}" alt="Logo 6" class="img-fluid" style="max-height: 100px;">
-        <img src="{{ asset('img/kinaadman.png') }}" alt="Logo 6" class="img-fluid" style="max-height: 200px;">
+    <div class="row my-5 px-5">
+        <div class="d-flex justify-content-center align-items-center flex-wrap gap-4">
+            <img src="{{ asset('img/cdo-seal.png') }}" alt="Logo 1" class="img-fluid" style="max-height: 150px;">
+            <img src="{{ asset('img/seallogo.png') }}" alt="Logo 2" class="img-fluid" style="max-height: 120px;">
+            <img src="{{ asset('img/risebig.png') }}" alt="Logo 3" class="img-fluid" style="max-height: 100px;">
+            <img src="{{ asset('img/logo1.png') }}" alt="Logo 4" class="img-fluid" style="max-height: 100px;">
+            <img src="{{ asset('img/tourismlogo.png') }}" alt="Logo 5" class="img-fluid" style="max-height: 100px;">
+            <img src="{{ asset('img/ictlogo.png') }}" alt="Logo 6" class="img-fluid" style="max-height: 100px;">
+            <img src="{{ asset('img/kinaadman.png') }}" alt="Logo 6" class="img-fluid" style="max-height: 200px;">
+        </div>
     </div>
-</div>
-
-
-    
-
-
-
-
-
-
     <!-- Bootstrap Bundle JS (includes Popper) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
-
-
-
-
-
-
-{{-- modal --}}
-
-
-
-<div class="modal fade" id="fullscreenModal" tabindex="-1" aria-labelledby="fullscreenModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-fullscreen">
-    <div class="modal-content bg-dark p-0 border-0"> <!-- Optional: dark background -->
-      <div class="modal-header border-0">
-        <h5 class="modal-title text-white" id="fullscreenModalLabel">Joseph Paulo Miguel Suero</h5>
-        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body p-0 d-flex justify-content-center align-items-center">
-        {{-- <img src="{{ asset('img/sample2.jfif') }}" alt="Logo" class="w-100 h-100 object-fit-contain"> --}}
-
-
-        <img id="poster-image" src="" alt="Poster Output" class="img-fluid w-100 h-100 object-fit-contain">
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
-
-
-
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        const modal = document.getElementById('fullscreenModal');
-        const image = document.getElementById('poster-image');
-
-        document.querySelectorAll('.show-poster').forEach(button => {
-            button.addEventListener('click', () => {
-                const imageUrl = button.getAttribute('data-image');
-                image.src = imageUrl;
-            });
-        });
-    });
-</script>
-
-
-
-
+    </body>
+    {{-- modal --}}
 </html>
