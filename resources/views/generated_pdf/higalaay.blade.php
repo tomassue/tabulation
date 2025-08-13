60aba8757368891279307dd666efc79d76111602
Tom Abella <tomabella64@gmail.com>
2025-08-13 15:01:11 +0800

    08-13-2025
    Corrected table header and row formatting for consistency and readability in the Higalaay PDF Blade view. Also updated the Kahigayunan event date from August 08, 2025 to August 13, 2025.

--- resources/views/generated_pdf/higalaay.blade.php
+++ resources/views/generated_pdf/higalaay.blade.php
@@ -71,19 +71,19 @@
         {{-- <tr>
             <td class="text-start" style="vertical-align: top;" width="30%">
                 <img src="{{ convert_image(public_path() . '/img/cdo_email.png') }}" width="80">
-                <img src="{{ convert_image(public_path() . '/img/goldencdo_email.png') }}" width="150">
+        <img src="{{ convert_image(public_path() . '/img/goldencdo_email.png') }}" width="150">
 
-            </td>
-            <td class="text-center">
-                <div style="font-size: 20pt;font-weight:bold;">HIGALAAY FESTIVAL</div>
-                <div style="font-size: 11pt;font-weight:bold;color: #26a75c;">CAGAYAN DE ORO at 75</div>
-                <div style="font-size: 12pt;">Proud of our Roots. Bold in our Dreams</div>
-                <div style="font-size: 9pt;">August 27, 2025</div>
-            </td>
-            <td class="text-end" width="30%">
-                <img src="{{ convert_image(public_path() . '/img/higalaay_email.png') }}" width="120">
-                <img src="{{ convert_image(public_path() . '/img/tourism_email.png') }}" width="75">
-            </td>
+        </td>
+        <td class="text-center">
+            <div style="font-size: 20pt;font-weight:bold;">HIGALAAY FESTIVAL</div>
+            <div style="font-size: 11pt;font-weight:bold;color: #26a75c;">CAGAYAN DE ORO at 75</div>
+            <div style="font-size: 12pt;">Proud of our Roots. Bold in our Dreams</div>
+            <div style="font-size: 9pt;">August 27, 2025</div>
+        </td>
+        <td class="text-end" width="30%">
+            <img src="{{ convert_image(public_path() . '/img/higalaay_email.png') }}" width="120">
+            <img src="{{ convert_image(public_path() . '/img/tourism_email.png') }}" width="75">
+        </td>
         </tr> --}}
         <tr>
             <td class="text-start" style="vertical-align: top;" width="30%">
@@ -94,7 +94,7 @@
                 <div style="font-size: 20pt;font-weight:bold;">KAHIGAYONAN 2025</div>
                 <div style="font-size: 18pt;">Skills Training and Expo</div>
                 <div style="font-size: 12pt;">Piyesta sa Dakbayan, Tabo sa Opportunidad Year 4</div>
-                <div style="font-size: 9pt;">August 08, 2025</div>
+                <div style="font-size: 9pt;">August 13, 2025</div>
             </td>
             <td class="text-end" width="30%">
                 <img src="{{ convert_image(public_path() . '/img/risebig.png') }}" width="150">
@@ -115,57 +115,57 @@
         <thead>
             <tr>
                 <th class="text-center p-2 bold" style="font-size:10pt;">RANK</th>
-                <th class="text-center  p-2 bold"style="font-size:10pt;">CONTESTANT</th>
+                <th class="text-center  p-2 bold" style="font-size:10pt;">CONTESTANT</th>
                 <th class="text-center p-2 bold" style="font-size:10pt;">NUMBER</th>
                 @foreach ($judges as $judge)
-                    <th class="text-center  p-2 bold"style="font-size:10pt;">
-                        {{ $judge->judge }}
-                    </th>
+                <th class="text-center  p-2 bold" style="font-size:10pt;">
+                    {{ $judge->judge }}
+                </th>
                 @endforeach
-                <th class="text-center  p-2 bold"style="font-size:10pt;">DEDUCTION</th>
-                <th class="text-center  p-2 bold"style="font-size:10pt;">TOTAL</th>
+                <th class="text-center  p-2 bold" style="font-size:10pt;">DEDUCTION</th>
+                <th class="text-center  p-2 bold" style="font-size:10pt;">TOTAL</th>
             </tr>
         </thead>
         @foreach ($participants as $position => $item)
-            <tr>
-                @if ($item->current_rank <= $winner)
-                    @php
-                        $font = 'font-weight: bold;font-size: 12pt;background-color: #26a75c;color: white;';
-                    @endphp
+        <tr>
+            @if ($item->current_rank <= $winner)
+                @php
+                $font='font-weight: bold;font-size: 12pt;background-color: #26a75c;color: white;' ;
+                @endphp
                 @else
-                    @php
-                        $font = 'font-size: 12pt;';
-                    @endphp
+                @php
+                $font='font-size: 12pt;' ;
+                @endphp
                 @endif
                 <td class="text-center" style="{{ $font }}">
-                    <span style="{{ $font }}">{{ bong_ordinal($item->current_rank) }}</span>
+                <span style="{{ $font }}">{{ bong_ordinal($item->current_rank) }}</span>
                 </td>
                 <td class="text-start" style="padding:5px;color:black;{{ $font }};">{{ $item->participant }}</td>
                 <td class="text-center" style="padding:5px;color:black;{{ $font }};">{{ $item->participant_no }}</td>
                 @foreach ($judges as $judge)
-                    <td class="text-center" style="padding:5px;color:black;{{ $font }};">{{ $item->getHigalaayScoreByJudge($judge->id, $category) }}</td>
+                <td class="text-center" style="padding:5px;color:black;{{ $font }};">{{ $item->getHigalaayScoreByJudge($judge->id, $category) }}</td>
                 @endforeach
 
                 <td class="text-center" style="font-weight: bold;color:black;{{ $font }}">{{ $item->higalaayDeduction?->deduction == 0 ? '-' : bong_format($item->higalaayDeduction?->deduction) }}</td>
                 <td class="text-center" style="font-weight: bold;color:black;{{ $font }}">{{ bong_format($item->averageHigalaay($category)) }}</td>
-            </tr>
+        </tr>
         @endforeach
     </table>
     <table class="table" style="margin-top: 100px;">
         <tr>
             @foreach ($judges as $judge)
-                <td style="text-align: right;margin-bottom:100px; vertical-align: top;">
-                    <label for="">JUDGE:</label>&nbsp;
-                </td>
-                <td style="text-align: center;">
-                    <span style="text-transform: uppercase;font-weight: bold">{{ $judge->judge }}</span>
-                    <div class="text-center p-2" style="border-top: 1px solid black;">
-                        Signature over printed name
-                    </div>
-                </td>
-                <td width="20px;">
-                    &nbsp;
-                </td>
+            <td style="text-align: right;margin-bottom:100px; vertical-align: top;">
+                <label for="">JUDGE:</label>&nbsp;
+            </td>
+            <td style="text-align: center;">
+                <span style="text-transform: uppercase;font-weight: bold">{{ $judge->judge }}</span>
+                <div class="text-center p-2" style="border-top: 1px solid black;">
+                    Signature over printed name
+                </div>
+            </td>
+            <td width="20px;">
+                &nbsp;
+            </td>
             @endforeach
         </tr>
     </table>
